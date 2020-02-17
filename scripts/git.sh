0148a5e5903039ed6215440b3b270625b53442d1
an-lee <an.lee.work@gmail.com>
2020-02-17 12:12:00 +0800

    Fix git.sh

--- scripts/git.sh
+++ scripts/git.sh
@@ -12,6 +12,6 @@ git config --global user.name an-lee
 git config --global user.email an.lee.work@gmail.com
 git config --global alias.co checkout
 git config --global alias.cm commit
-git config --global alias.br bransh
+git config --global alias.br branch
 git config --global alias.st status
 git config --global alias.l log --pretty=format:"%h <%an> %ar %s"
