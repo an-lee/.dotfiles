4f9c3aad8b54b99505d9673e5528ff509ed7db50
an-lee <an.lee.work@gmail.com>
2020-05-06 10:53:06 +0800

    :sparkles: (vim) add coc extensions

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -10,10 +10,14 @@ ln -s ~/.dotfiles/.vimrc ~/.vimrc
 [ -f ~/.vimplugs ] && rm ~/.vimplugs
 ln -s ~/.dotfiles/.vimplugs ~/.vimplugs
 
+# symlink coc extensions
+[ -f ~/.config/coc/extensions/package.json ] && rm ~/.config/coc/extensions/package.json
+ln -s ~/.dotfiles/coc/extensions/package.json ~/.config/coc/extensions/package.json
+
 # clean & install plugins
 vim -c 'PlugUpgrade|PlugClean|PlugInstall|PlugUpdate|qa'
 
 # install coc extensions
-vim -c 'CocInstall -sync coc-tsserver coc-json coc-html coc-prettier coc-yaml \
-  coc-solargraph coc-pairs coc-wxml coc-css coc-tslint coc-highlight \
-  coc-angular|q'
+# vim -c 'CocInstall -sync coc-tsserver coc-json coc-html coc-prettier coc-yaml \
+#   coc-solargraph coc-pairs coc-wxml coc-css coc-tslint coc-highlight \
+#   coc-angular|q'
