54ddc955ed822b372eeba23585d087815a8c0de0
an-lee <an.lee.work@gmail.com>
2020-06-22 11:06:27 +0800

    Setup .gitconfig

--- scripts/git.sh
+++ scripts/git.sh
@@ -4,14 +4,7 @@
 # symlink .gitigore
 [ -f ~/.gitignore ] && rm ~/.gitignore
 ln -s ~/.dotfiles/.gitignore ~/.gitignore
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
 # git config
 git config --global core.excludesfile '~/.gitignore'
-git config --global core.editor vim
-git config --global user.name an-lee
-git config --global user.email an.lee.work@gmail.com
-git config --global alias.co checkout
-git config --global alias.cm commit
-git config --global alias.br branch
-git config --global alias.st status
-git config --global alias.l log --pretty=format:"%h <%an> %ar %s"
